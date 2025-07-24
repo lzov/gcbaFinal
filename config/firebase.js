17f47799808e1ca6d2c5cc7b6a44a8d7c98f2a5b
lzov <lizovegab@gmail.com>
2025-07-24 04:42:54 +0000

    another one

--- config/firebase.js
+++ config/firebase.js
@@ -1,28 +1,12 @@
 import admin from 'firebase-admin';
-import { readFileSync } from 'fs';
-import { dirname, join } from 'path';
-import { fileURLToPath } from 'url';
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const serviceAccount = {
-  type: process.env.FIREBASE_TYPE,
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  client_id: process.env.FIREBASE_CLIENT_ID,
-  auth_uri: process.env.FIREBASE_AUTH_URI,
-  token_uri: process.env.FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
-  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
-};
-        
+import { firebaseConfig } from './config.js';
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(firebaseConfig),
 });
 
 const db = admin.firestore();
 
 export { db };
-export const productsCollection = db.collection('productos'); 
+export const productsCollection = db.collection('productos');
+export default admin;
